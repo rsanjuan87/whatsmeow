bc4f978b7677f54af5a35c16d0eb0638840780d5
San Juan <rsanjuan1987@gmail.com>
2023-10-20 11:40:52 +0200

    fix

--- types/newsletter.go
+++ types/newsletter.go
@@ -13,7 +13,7 @@ import (
 
 	"go.mau.fi/util/jsontime"
 
-	waProto "go.mau.fi/whatsmeow/binary/proto"
+	waProto "github.com/rsanjuan87/whatsmeow/binary/proto"
 )
 
 type NewsletterVerificationState string
